5ddc69359cae60e71a462e21aebfe41f709e599a
jxun_h <its1108@kakao.com>
2024-04-30 17:51:25 +0900

    메인 컨트롤러 작성, 로그인 페이지 작성

--- src/main/java/its/backend/web/Controller/user/UserController.java
+++ src/main/java/its/backend/web/Controller/user/UserController.java
@@ -1,6 +1,5 @@
 package its.backend.web.Controller.user;
 
-import its.backend.global.config.error.exception.BaseException;
 import its.backend.global.response.BaseResponse;
 import its.backend.web.Service.UserService;
 import its.backend.web.dto.SignInReq;
@@ -8,12 +7,9 @@ import its.backend.web.dto.SignInRes;
 import its.backend.web.dto.SignUpReq;
 import its.backend.web.dto.SignUpRes;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static its.backend.global.config.error.ErrorCode.BAD_REQUEST;
-import static its.backend.global.response.BaseResponseStatus.SUCCESS;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,13 +17,9 @@ import static its.backend.global.response.BaseResponseStatus.SUCCESS;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/test")
-    public ResponseEntity<BaseResponse> userTest() {
-        try {
-            return ResponseEntity.ok(new BaseResponse<>(SUCCESS));
-        } catch (BaseException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(new BaseResponse<>(BAD_REQUEST));
-        }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 
     @PostMapping("/users/auth/signUp")
