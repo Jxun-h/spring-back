1ef2e3bf450bb8ccac913acbc9167ec3e9246e67
jxun_h <its1108@kakao.com>
2024-05-03 16:47:45 +0900

    로그인 Cors 처리

---
+++ src/main/java/its/backend/global/config/WebConfig.java
@@ -0,0 +1,17 @@
+package its.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:8080") // 허용할 오리진 패턴
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 자격 증명 허용 여부
+    }
+}
